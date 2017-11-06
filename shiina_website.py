b0eae7233b5a08c780f2afbbc94d9b1b74ed021f
ShiinaOrez <shiina_orez@qq.com>
2017-11-06 11:18:08 +0800

    2017.11.06 morning

--- shiina_website.py
+++ shiina_website.py
@@ -2,9 +2,17 @@ from flask import Flask,render_template,session
 
 app=Flask(__name__)
 
-@app.route('/shiina_website')
+@app.route('/shiina_website',methods=['GET','POST'])
 def shiina_website():
 	return render_template("shiina_website_index.html")
+
+@app.route('/shiina_website/login',methods=['GET','POST'])
+def shiina_website_login():
+	return render_template("shiina_website_login.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
 
 if __name__=="__main__":
 	app.run(debug=True)
