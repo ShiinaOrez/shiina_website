18b36e5e283486bfeb9a4d95fff0f188d6203d71
ShiinaOrez <shiina_orez@qq.com>
2017-11-25 21:34:47 +0800

    app password_hash fixed

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -1,12 +1,22 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
  	__tablename__='users'
 	id=db.Column(db.Integer,primary_key=True)
 	username=db.Column(db.String(20),unique=True)
     	password=db.Column(db.String(20))
+        password_hash=db.Column(db.String(128))
 	useremail=db.Column(db.String(30),unique=True)
 	texts=db.relationship('Text',backref='user')
+        @property
+        def password(self):
+            raise AttributeError('password is not a readable attribute')
+	@password.setter
+        def password(self,password):
+            self.password_hash=generate_password_hash(password)
+        def verify_password(self,password):
+            return check_password_hash(self.password_hash,password)
 
 class Text(db.Model):
 	__tablename__='texts'
